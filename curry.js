b31026b37111a1d38249b34eeb6b04075a3e0e20
Jason Reeder <info@jasonreeder.com>
2017-02-24 14:31:26 -0800

    Monkey patch curry

--- curry.js
+++ curry.js
@@ -20,4 +20,22 @@ let curriedSum = (numArgs) => {
 };
 
 const sum = curriedSum(4);
-console.log(sum(5)(30)(20)(1)); // => 56
+// console.log(sum(5)(30)(20)(1)); // => 56
+
+Function.prototype.myCurry = function(numArgs) {
+  let numbers = [];
+
+  const _myCurry = (newNum) => {
+    numbers.push(newNum);
+
+    if (numbers.length === numArgs) {
+      return this(numbers); //this refers to sumNums
+    } else {
+      return _myCurry;
+    }
+  };
+
+  return _myCurry;
+};
+
+console.log(sumNums.myCurry(3)(4)(20)(6));
