ebaefd6123c18c9fec8d57f28590d5e81dfd8a31
Jason Reeder <info@jasonreeder.com>
2017-02-24 11:12:03 -0800

    First attempt bind

---
+++ bind.js
@@ -0,0 +1,23 @@
+Function.prototype.myBind = function (boundObject) {
+  return (sound) => { this.apply(boundObject, sound);
+  };
+};
+
+class Cat {
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+const says = function(sound) {
+  console.log(`${this.name} says ${sound}!`);
+  return true;
+};
+
+const markov = new Cat("Markov");
+const breakfast = new Cat("Breakfast");
+
+const markovBoundSays = says.myBind(markov);
+// const markovBoundSays = says.bind(markov);
+
+markovBoundSays("meow");
